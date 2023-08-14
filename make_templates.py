c769c227c2281242ea16fbf7678adf026909b239
banteg <4562643+banteg@users.noreply.github.com>
2023-08-15 00:48:47 +0400

    make templates

---
+++ make_templates.py
@@ -0,0 +1,14 @@
+TEMPLATE = '''\
+# @version {}
+
+n: public(uint256)
+
+@external
+def set_n(to: uint256):
+    assert to == 42  # dev: wrong answer
+    self.n = to
+'''
+
+for version in ['0.3.7', '0.3.8', '0.3.9', '0.3.10']:
+    with open(f'contracts/Template{version.replace(".", "")}.vy') as f:
+        f.write(TEMPLATE.format(version))
